a5ec198e9cd818b6f62eef2837ab74f4dddc9177
Jake Traut <jaketraut@gmail.com>
2017-04-25 11:10:55 -0600

    started on initializing vars and functions

--- workspace/Tetris/src/Game.java
+++ workspace/Tetris/src/Game.java
@@ -1,4 +1,46 @@
 
 public class Game {
-
+	public int timer;
+	public String currentPlayer;
+	public int[] currentPieces;
+	public int score;
+	public static int windowHeight;
+	public static int windowWidth;
+	public boolean[][] border = new boolean[30][10]; 
+	
+	public void startTimer(int timer)
+	{
+		
+	}
+	
+	public void stopTimer(int timer)
+	{
+		
+	}
+	
+	public void displayPauseMenu(boolean pressedPause)
+	{
+		
+	}
+	
+	public void hidePauseMenu(boolean endPause)
+	{
+		
+	}
+	
+	public int[] endGame(boolean gameOver)
+	{
+		
+	}
+	
+	public int initializeScore()
+	{
+		//or nah
+	}
+	
+	public Piece showNextPiece()
+	{
+		
+	}
+	
 }
